6579e1baa32e1c38f4bc79503987a53505775fa2
N Hariharan <hariharannandha2005@gmail.com>
2026-01-01 23:52:00 +0530

    Update UserService to use new backend URL

--- frontend-angular/src/app/services/user.service.ts
+++ frontend-angular/src/app/services/user.service.ts
@@ -1,31 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { User } from '../models/user.model';   // <-- Make sure this path exists
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private apiUrl = "https://hariharan202135-bs1765869829324-task4.onrender.com";  // Backend URL
+  // ⛔ Old:
+  // private apiUrl = "http://localhost:5000";
+
+  // ✅ New Render Backend URL
+  private apiUrl = "https://hariharan202135-bs1765869829324-task4.onrender.com";
 
   constructor(private http: HttpClient) { }
 
-  // GET users (returns list of users)
+  // Get all users
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(`${this.apiUrl}/users`);
   }
 
-  // ADD user
-  addUser(data: User): Observable<any> {
+  // Add a new user
+  addUser(data: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/users`, data);
   }
 
-  // DELETE user
+  // Delete user by id
   deleteUser(id: string): Observable<any> {
     return this.http.delete(`${this.apiUrl}/users/${id}`);
   }
-
 }
-
